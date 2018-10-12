e0f77f097bfaec06d8b7595f4c10d46a880f1cfc
nopparat231 <23.noop@gmail.com>
2018-10-12 21:21:53 +0700

    up inv

---
+++ print_report.php
@@ -0,0 +1,236 @@
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" />
+<style type="text/css">
+#form1 table tr th h3 {
+	text-align: center;
+}
+</style>
+<form id="form1" name="form1" method="post" action="">
+  <table width="1081" height="374" border="0">
+    <tr>
+      <th width="361" scope="row"><img src="img/LOGO-CHATA.png" width="150" height="150" /></th>
+      <th width="704" align="left" nowrap="nowrap"><h3><strong>บริษัท CHATA จํากัด</strong></h3>
+        <p> 414 ซอย สลีม36 ถนนสลีม แขวงสุรยิวงศ์เขตบางรกั กรุงเทพ 10500 </p>
+<p>  414 Silom36 silomRd. Suriyawong Bangrak Bangkok (Thailand) 10500</p>
+        <p> โทร. 081-493-3899 เลขประจําตวัผูเสียภาษอีากร 110200836647 </p>
+        <p>&nbsp;</p>
+        <h4> ใบเสรจ็ รบัเงิน / ใบกํากบั ภาษี   </h4>
+      <h4>ORIGINAL RECEIPT / TAX INVOICE </h4></th>
+    </tr>
+    <tr>
+      <th height="195" scope="row"><table width="489" border="0" align="left">
+        <tr>
+          <th width="195" align="left" scope="row"><p>&nbsp; &nbsp; นามลูกค้า</p></th>
+          <td width="247" align="left" valign="bottom"><p>&nbsp;&nbsp;  บ.อิสระ คอเลคชั่น    จำกัด</p></td>
+        </tr>
+        <tr>
+          <th align="left" scope="row"><p>&nbsp; &nbsp; ที่อยู่</p></th>
+          <td align="left" valign="bottom"><p>
+            <label>dfgdfgdfgdfgdfg</label>
+            &nbsp;
+            <label>dfgdfgdfgdfgdfg</label>
+&nbsp;
+<label>dfgdfgdfgdfgdfg</label>
+&nbsp;</p></td>
+        </tr>
+        <tr>
+          <th align="left" scope="row"><p>&nbsp; &nbsp; เลขประจำตัวผู้เสียภาษี</p></th>
+          <td align="left" valign="bottom"> <p> 12321321213</p></td>
+        </tr>
+      </table>        <h3>&nbsp;</h3></th>
+      <td><table width="300" border="0" align="right">
+        <tr>
+          <th scope="row">เลขที่</th>
+          <td>CH000001</td>
+        </tr>
+        <tr>
+          <th scope="row">วันที่</th>
+          <td>18/07/2018</td>
+        </tr>
+      </table></td>
+    </tr>
+  </table>
+  <table width="2" height="204" border="1" cellpadding="0" cellspacing="0">
+    <col width="36" />
+    <col width="23" />
+    <col width="28" />
+    <col width="123" />
+    <col width="94" />
+    <col width="68" />
+    <col width="10" />
+    <col width="8" />
+    <col width="22" />
+    <col width="59" />
+    <col width="57" />
+    <col width="22" />
+    <col width="54" />
+    <col width="9" />
+    <col width="45" />
+    <col width="13" />
+    <col width="8" />
+    <col width="13" />
+    <col width="63" />
+    <col width="41" />
+    <col width="127" />
+    <col width="20" />
+  </table>
+  <table width="1081" border="1" cellpadding="0" cellspacing="0">
+    <tr align="center" class="bg-primary">
+      <td width="103" nowrap="nowrap" scope="row">ลำดับ</td>
+      <td width="156" nowrap="nowrap">รหัสสินค้า</td>
+      <td width="295" nowrap="nowrap">รายละเอียด</td>
+      <td width="47" nowrap="nowrap">ไซส์</td>
+      <td width="80" nowrap="nowrap">จำนวน</td>
+      <td width="88" nowrap="nowrap">หน่วย</td>
+      <td width="124" nowrap="nowrap">ราคา/หน่วย</td>
+      <td width="170" nowrap="nowrap">จำนวนเงิน</td>
+    </tr>
+    <tr>
+      <td align="center" scope="row">1</td>
+      <td align="center">g0154</td>
+      <td align="center">แบบ 0154</td>
+      <td align="center">s</td>
+      <td align="center">2</td>
+      <td align="center">วง</td>
+      <td align="center">5900</td>
+      <td align="right">11800</td>
+    </tr>
+    <tr>
+      <th align="center" scope="row">&nbsp;</th>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+    </tr>
+    <tr>
+      <th align="center" scope="row">&nbsp;</th>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+    </tr>
+    <tr>
+      <th align="center" scope="row">&nbsp;</th>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+    </tr>
+    <tr>
+      <th align="center" scope="row">&nbsp;</th>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+    </tr>
+    <tr>
+      <th align="center" scope="row">&nbsp;</th>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+    </tr>
+    <tr>
+      <th align="center" scope="row">&nbsp;</th>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+    </tr>
+    <tr>
+      <th align="center" scope="row">&nbsp;</th>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+    </tr>
+    <tr>
+      <th align="center" scope="row">&nbsp;</th>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+    </tr>
+  </table>
+  <table width="1081" border="1" cellpadding="0" cellspacing="0">
+    <tr>
+      <th width="70%" align="left" scope="row">&nbsp;ตัวอักษร. : (หนึ่งหมื่นสองพันหกร้อยยี่สิบหกบาทถ้วน)</th>
+      <td width="20%"> &nbsp;รวมเงิน<br />
+      &nbsp;TOTAL</td>
+      <td width="10%" align="right">11800</td>
+    </tr>
+    <tr>
+      <th align="left" valign="bottom" scope="row"> &nbsp;<input type="checkbox" name="checkbox" id="checkbox" />
+        <label for="checkbox"></label>
+      เงินสด ................................................... บาท</th>
+      <td>&nbsp;ส่วนลด<br />
+      &nbsp;DISCOUNT</td>
+      <td align="right">0%</td>
+    </tr>
+    <tr>
+      <th align="left" valign="bottom" scope="row"> &nbsp;<input type="checkbox" name="checkbox2" id="checkbox2" />
+        <label for="checkbox2"></label>
+      เงินโอน .................................................. บาท</th>
+      <td>&nbsp;มูลค่าสินค้าหลังหักส่วนลด<br />
+      &nbsp;TOTAL AMOUNT AFTER &nbsp;DISCOUNT</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <th align="left" scope="row">&nbsp;</th>
+      <td>&nbsp;ภาษีมูลค่าเพิ่ม<br />
+      &nbsp;VAT</td>
+      <td align="right">11800</td>
+    </tr>
+    <tr>
+      <th align="left" scope="row">&nbsp;</th>
+      <td>&nbsp;ยอดรวมสุทธิ<br />
+      &nbsp;GRAND TOTAL</td>
+      <td align="right"><strong>11800</strong></td>
+    </tr>
+  </table>
+  <p>&nbsp;</p>
+  <table width="691" align="center" cellpadding="0" cellspacing="0">
+    <tr>
+      <td colspan="22" width="943"><h4>กรณีชำระด้วยเช็ค ใบเสร็จฯ นี้จะสมบูรณ์ต่อเมื่อบริษัทฯ    ได้รับเงินแล้วเท่านั้น</h4></td>
+    </tr>
+  </table>
+  <table width="1081" border="1" cellpadding="0" cellspacing="0">
+    <tr>
+      <th height="53" valign="bottom" scope="row"><p> &nbsp;ผู้รับสินค้า .................................................................</p></th>
+      <th valign="bottom"><p> &nbsp;ผู้ส่งสินค้า .................................................................</p></th>
+      <th align="center" valign="bottom"><p> &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;ในนาม บริษัท CHATA จำกัด</p></th>
+    </tr>
+    <tr>
+      <th height="63" valign="bottom" scope="row"><p> &nbsp;ลงวันที่  .................................................................</p></th>
+      <th valign="bottom"><p>&nbsp;ลงวันที่  .................................................................</p></th>
+      <th align="center" valign="bottom"><p>&nbsp;ลงวันที่  .................................................................</p></th>
+    </tr>
+  </table>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+</form>
